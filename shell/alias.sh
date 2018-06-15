600ea24ce62c74caec4c07c257eaeaed2ce8f49f
Riza Suminto <riza.on@gmail.com>
2018-06-14 23:23:48 -0700

    break down zshrc

---
+++ shell/alias.sh
@@ -0,0 +1,33 @@
+# Use colors in coreutils utilities output
+alias grep='grep --color'
+
+# ls aliases
+alias ll='ls -lah'
+alias la='ls -A'
+alias l='ls'
+
+alias e='vim'
+alias m='make'
+alias mall='make all'
+alias diff='colordiff'
+alias reload='source ~/.zshrc'
+alias zshrc='$EDITOR ~/.zshrc'
+
+# git aliases
+alias gits='git status'
+alias gitmt='git mergetool'
+
+# cd aliases
+alias dotfiles='cd $DOTFILES'
+alias hp='cd $HADOOP_PREFIX'
+alias hconf='cd $HADOOP_CONF_DIR'
+alias wp='cd $ECLIPSE_WORKSPACE'
+alias dirpapers='cd $DIRPAPERS'
+alias jira='cd $JIRA_REPO'
+alias ucare='cd $UCARE_DIR'
+alias copaper='cd $UCARE_DIR/futurexx-cobe-svn/v0'
+alias cotrace='cd $UCARE_DIR/cobe-analysis-svn'
+alias cobugs='cd $JIRA_REPO/special-bugs/co-cascadingOutage'
+alias cocode='cd $ECLIPSE_WORKSPACE/cobe'
+alias candidacy='cd $UCARE_DIR/riza-candidacy-exam'
+
---
+++ shell/env.sh
@@ -0,0 +1,18 @@
+export CLICOLOR=1
+export LSCOLORS=GxFxCxDxBxegedabagaced
+
+export UCARE_DIR=$HOME/Documents/UCARE
+export DIRPAPERS=$UCARE_DIR/DIR-PAPERS
+export RESEARCH=$UCARE_DIR/blogs
+export JIRA_REPO=$UCARE_DIR/jira-study-db-svnrepo
+export ECLIPSE_WORKSPACE=$HOME/Documents/workspace
+export EDITOR=vim
+export DOTFILES=$HOME/dotfiles
+#export TEXINPUTS=~/pgfplots//:
+
+# local bin
+pathadd ~/bin
+
+# macports
+pathadd /opt/local/sbin
+pathadd /opt/local/bin
