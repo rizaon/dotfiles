4338ab4b26d2dfe40013c42adcfb0158d4514f3d
Riza Suminto <riza.on@gmail.com>
2018-06-20 07:06:02 -0700

    add generic extention based search

--- shell/function.sh
+++ shell/function.sh
@@ -1,11 +1,11 @@
 function setjdk() {
   if [ $# -ne 0 ]; then
-   removeFromPath '/System/Library/Frameworks/JavaVM.framework/Home/bin'
-   if [ -n "${JAVA_HOME+x}" ]; then
-    removeFromPath $JAVA_HOME
-   fi
-   export JAVA_HOME=`/usr/libexec/java_home -v $@`
-   export PATH=$JAVA_HOME/bin:$PATH
+    removeFromPath '/System/Library/Frameworks/JavaVM.framework/Home/bin'
+    if [ -n "${JAVA_HOME+x}" ]; then
+      removeFromPath $JAVA_HOME
+    fi
+    export JAVA_HOME=`/usr/libexec/java_home -v $@`
+    export PATH=$JAVA_HOME/bin:$PATH
   fi
 }
 
@@ -14,8 +14,51 @@ function removeFromPath() {
 }
 
 pathadd() {
-    if [ -d "$1" ] && [[ ":$PATH:" != *":$1:"* ]]; then
-        PATH="$1${PATH:+":$PATH"}"
-    fi
+  if [ -d "$1" ] && [[ ":$PATH:" != *":$1:"* ]]; then
+    PATH="$1${PATH:+":$PATH"}"
+  fi
 }
 
+
+# search utillities
+gext() {
+  if [ $# -lt 2 ]; then
+    echo ""
+    echo "  find . -name \"*.java\" -exec grep (something) '{}' \; -print  | cat -n"
+    echo ""
+    return
+  fi
+
+  ext="\.$1"
+  shift
+  srcstr="$1"
+  shift
+
+  while (( $# )); do
+    echo $1
+    srcstr="$srcstr.$1"
+    shift
+  done
+
+  # find . -name "*.java" -exec grep "$*" '{}' \; -print  | cat -n
+
+  # find . -name "*.java" -exec grep -i "$srcstr" '{}' \; -print  | cat -n
+
+  echo ""
+
+  find . -type f -name "*$ext" -exec \
+  grep -i -n "$srcstr" '{}' \; -print  | \
+  sed "s/^/    /" | \
+  sed "s/^    \.\//-------------------------------------------/" | \
+  grep -i --color ".*$ext\|$srcstr"
+
+  echo ""
+}
+
+gsh() {
+  gext sh "$@"
+}
+
+gjava() {
+  gext java "$@"
+}
