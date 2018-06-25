0bf56aa667b441e1995b091812d611f1bd0f5247
Riza Suminto <riza.on@gmail.com>
2018-06-25 10:18:19 -0700

    export LS_COLORS for linux

--- shell/env.sh
+++ shell/env.sh
@@ -1,5 +1,8 @@
 export CLICOLOR=1
+# mac
 export LSCOLORS=GxFxCxDxBxegedabagaced
+# linux
+export LS_COLORS="di=34:ln=35:so=32:pi=33:ex=31:bd=34;46:cd=34;43:su=30;41:sg=30;46:tw=30;42:ow=30;43"
 
 export UCARE_DIR=$HOME/Documents/UCARE
 export DIRPAPERS=$UCARE_DIR/DIR-PAPERS
