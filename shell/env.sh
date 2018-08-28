b8150c537e9b377f97b2492c5771c02d99df7ceb
Riza Suminto <riza.on@gmail.com>
2018-08-28 13:44:03 -0500

    update bunch of new scripts

--- shell/env.sh
+++ shell/env.sh
@@ -4,6 +4,8 @@ export LSCOLORS=GxFxCxDxBxegedabagaced
 # linux
 export LS_COLORS="di=34:ln=35:so=32:pi=33:ex=31:bd=34;46:cd=34;43:su=30;41:sg=30;46:tw=30;42:ow=30;43"
 
+export MAVEN_OPTS="-Dhttps.protocols=TLSv1.2"
+
 export UCARE_DIR=$HOME/Documents/UCARE
 export DIRPAPERS=$UCARE_DIR/DIR-PAPERS
 export RESEARCH=$UCARE_DIR/blogs
--- shell/function.sh
+++ shell/function.sh
@@ -27,3 +27,6 @@ gjava() {
   gext java "$@"
 }
 
+gphp() {
+  gext php "$@"
+}
