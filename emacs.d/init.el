81fd8882c3ac3b03232e887ce62a3917ec47b9ef
Riza Suminto <riza.on@gmail.com>
2018-09-18 13:17:39 -0500

    put all emacs stuff in emacs.d

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -1,4 +1,11 @@
-(setq load-path (cons "~/.emacs.path/modes-el/" load-path))
+;; Load path etc.
+(setq dotfiles-dir (file-name-directory
+                    (or (buffer-file-name) load-file-name)))
+(add-to-list 'load-path (concat dotfiles-dir "/init.d"))
+(add-to-list 'load-path (concat dotfiles-dir "/modes-el"))
+
+;; already loaded on top
+;; (setq load-path (cons "~/.emacs.d/modes-el/" load-path))
 (autoload 'markdown-mode "markdown-mode-modified.el" "..." t)
 (setq auto-mode-alist (cons '("\\.txt$" . markdown-mode) auto-mode-alist))
 (setq auto-mode-alist (cons '("\\.blog$" . markdown-mode) auto-mode-alist))
@@ -7,7 +14,7 @@
 (setq sentence-end-double-space nil)
 (setq-default fill-column 80)
 
-
-(setq load-path (cons "~/.emacs.path/init.d/" load-path))
+;; already loaded on top
+;; (setq load-path (cons "~/.emacs.d/init.d/" load-path))
 (require 'setup-bindings)
 (require 'darwin)
