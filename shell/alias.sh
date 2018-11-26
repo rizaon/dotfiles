473c55a267b2f1ac4d1992f91c98a1352ed103b4
Riza Suminto <riza.on@gmail.com>
2018-11-25 22:40:08 -0600

    add dcps alias

--- shell/alias.sh
+++ shell/alias.sh
@@ -47,4 +47,5 @@ alias dcup='docker-compose up'
 alias dcdown='docker-compose down'
 alias dcbuild='docker-compose build --no-cache'
 alias dcex='docker-compose exec'
+alias dcps='docker-compse ps'
 
