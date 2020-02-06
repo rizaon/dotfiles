f89baa012868480474bde5828fdf71f07563d826
Riza Suminto <riza.on@gmail.com>
2020-02-05 17:25:03 -0800

    update bin/setup-centos7.sh

--- bin/setup-centos7.sh
+++ bin/setup-centos7.sh
@@ -1,24 +1,20 @@
 #!/usr/bin/env bash
 
-# ATTENTION: You need to setup ssh-agent and add private key first before running this script
-# eval `ssh-agent`
-# ssh-add -k ~/.ssh/your-private-key
-
 # setup git
-sudo yum remove git*
+sudo yum -y remove git*
 sudo yum -y install  https://centos7.iuscommunity.org/ius-release.rpm
 sudo yum -y install  git2u-all
 git --version
 
 # install some tools
-sudo yum -y install htop tmux zsh
+sudo yum -y install vim htop tmux zsh
 
 # install dotfiles
 cd ~/
-git clone git@github.infra.cloudera.com:rizaon/dotfiles.git
+git clone https://github.com/rizaon/dotfiles.git
 cd dotfiles
 ./install
 
 # change shell
-sudo sed -i 's|/home/systest:/bin/bash|/home/systest:/bin/zsh|g' /etc/passwd
+sudo sed -i "s|/home/$USER:/bin/bash|/home/$USER:/bin/zsh|g" /etc/passwd
 
