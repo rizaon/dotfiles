7456c20b8082899f9ff129d8864387c6fc332992
Riza Suminto <riza.on@gmail.com>
2020-03-04 16:03:59 -0800

    update cloudera specific functions

--- shell/cloudera.sh
+++ shell/cloudera.sh
@@ -14,6 +14,20 @@ branch-master() {
     git fetch asf-gerrit && git checkout -b $1 asf-gerrit/master
 }
 
+# fetch temporary AWS S3 key
+fetch-aws-key() {
+    ssh s3@cloudcat.infra.cloudera.com impaladev
+}
+
 # rebase based on asf-gerrit/master
 alias "rebase-master"="git fetch asf-gerrit && git rebase -i asf-gerrit/master"
 
+ide-impala() {
+    cd $IMPALA_HOME
+    source $IMPALA_HOME/bin/impala-config.sh
+    if [[ "$OSTYPE" == "linux-gnu" ]]; then
+        clion.sh $IMPALA_HOME
+    elif [[ "$OSTYPE" == "darwin"* ]]; then
+        clion $IMPALA_HOME
+    fi
+}
